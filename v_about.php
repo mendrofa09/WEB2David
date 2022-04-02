6db186c96eae3a9ddf86ad1a7ade848ccf4a65f3
mendrofa09 <101868819+mendrofa09@users.noreply.github.com>
2022-04-02 23:00:51 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,30 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : David Utusan</li>
+        <li>Nama Belakang : Mendrofa</li>
+    </ul>
+    <br>
+        <h4>Alamat</h4>
+        <ul type="none">
+        <li> Jalan Imam Bonjol 23</li>
+    </ul>
+ 
+        <h4>Tempat Lahir</h4>
+        <ul type="none">
+        <li>Tangerang</li>
+    </ul>
+
+        <h4>Olah Raga Favorit</h4>
+        <ul type="square">
+        <li>Bulu Tangkis</li>
+        <li>Sepak Bola</li>
+    </ul>
+</section>
+
+
+
+
+
+
